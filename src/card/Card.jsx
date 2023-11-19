7eb8e711ff1d8bdc7562deb808844286e73c95b3
PoojaD12345 <dhuripooja01@gmail.com>
2023-11-19 10:37:34 +0530

    unchanged

--- src/card/Card.jsx
+++ src/card/Card.jsx
@@ -12,7 +12,7 @@ const Card = () => {
           alt="user-profile"
           className="profile"
         />
-        <svg
+        {/* <svg
             xmlns="http://www.w3.org/2000/svg"
             className="wave1"
             viewBox="0 0 1440 320"
@@ -35,7 +35,7 @@ const Card = () => {
               fill-opacity="1"
               d="M0,256L80,256C160,256,320,256,480,229.3C640,203,800,149,960,138.7C1120,128,1280,160,1360,176L1440,192L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"
             ></path>
-          </svg>
+          </svg> */}
 
         <div className="icondiv">
           <img
